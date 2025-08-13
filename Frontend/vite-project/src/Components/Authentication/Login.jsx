b93bbcb7937f2aed70186d44c059d2931b240d5b
SHUBHANK MAHESHWARI <maheshwarishubhank2002@gmail.com>
2025-08-13 09:49:03 +0530

    updated login component

--- Frontend/vite-project/src/Components/Authentication/Login.jsx
+++ Frontend/vite-project/src/Components/Authentication/Login.jsx
@@ -18,14 +18,16 @@ const Login = () => {
     seform({ ...form, [name]: value });
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="flex flex-col w-[75vw] h-[100vh] justify-center items-center gap-10">
+      <h1 className='text-3xl font-bold'>Login</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-20 border-1 rounded-2xl p-10">
         <input
           type="text"
           placeholder="email.."
           name="email"
           value={form.email}
           onChange={handleChange}
+          className='border-1 rounded-lg p-3 text-center'
         />
         <input
           type="password"
@@ -33,8 +35,9 @@ const Login = () => {
           name="password"
           value={form.password}
           onChange={handleChange}
+          className='border-1 rounded-lg p-3 text-center'
         />
-        <button type="submit">Login</button>
+        <button type="submit" className='cursor-pointer border-2 bg-[#00AC4F] rounded-lg text-white p-3 hover:scale-110 transition-transform delay-150 duration-300'>Login</button>
       </form>
     </div>
   );
--- Frontend/vite-project/src/Components/Home.jsx
+++ Frontend/vite-project/src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import Login from './Authentication/Login'
 
 const Home = () => {
   return (
-    <div className='bg-black w-screen'>
-      
+    <div className='bg-black w-screen flex'>
+      <Sidebar/>
+      <Login/>
     </div>
   )
 }
