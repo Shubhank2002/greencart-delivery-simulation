83b63cc8c88e087e7ee2fe689e7daefcd68f6ab9
SHUBHANK MAHESHWARI <maheshwarishubhank2002@gmail.com>
2025-08-13 11:28:26 +0530

    new changes

--- Backend/Controllers/AuthController.js
+++ Backend/Controllers/AuthController.js
@@ -14,14 +14,14 @@ const Login = async (req, res) => {
           message: "User credentials are invalid, please write it again",
         });
       } else {
-        const token= CreateToken(email)
-        res.cookie('token',token,{
-            httpOnly:true,
-            secure:true,
-            maxAge: 60 * 60 * 1000,
-           
-        })
-        res.status(200).json({ message: "user logined successfully",token });
+        const token = CreateToken(email);
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "None",
+          maxAge: 60 * 60 * 1000,
+        });
+        res.status(200).json({ message: "user logined successfully", token });
       }
     }
   } catch (error) {}
@@ -29,26 +29,31 @@ const Login = async (req, res) => {
 
 const Signup = async (req, res) => {
   const { email, password } = req.body;
-  
+
   try {
     const isExists = await UserModel.findOne({ email });
     if (isExists) {
       res.status(404).json({ message: "user already exist, please login" });
-    }else{
-        const salt=await bcryptjs.genSalt(10)
-        const hash_password=await bcryptjs.hash(password,salt)
-        const new_user=await UserModel.create({email,password:hash_password})
-        const token= CreateToken(email)
-        res.cookie('token',token,{
-            httpOnly:true,
-            secure:true,
-            maxAge: 60 * 60 * 1000,
-            
-        })
-        
-        res.status(200).json({ message: "user created successfully",token,secret_key });
+    } else {
+      const salt = await bcryptjs.genSalt(10);
+      const hash_password = await bcryptjs.hash(password, salt);
+      const new_user = await UserModel.create({
+        email,
+        password: hash_password,
+      });
+      const token = CreateToken(email);
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        maxAge: 60 * 60 * 1000,
+      });
+
+      res
+        .status(200)
+        .json({ message: "user created successfully", token, secret_key });
     }
   } catch (error) {}
 };
 
-module.exports = { Login,Signup };
+module.exports = { Login, Signup };
