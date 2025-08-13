9f7221f3f4c63ae86c983df481c832ef05850ae9
SHUBHANK MAHESHWARI <maheshwarishubhank2002@gmail.com>
2025-08-13 09:38:36 +0530

    new changes

--- Backend/Utils/Token.js
+++ Backend/Utils/Token.js
@@ -1,15 +1,13 @@
-const jwt=require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-
-const CreateToken=(email)=>{
-    
-    
-    try {
-        const token= jwt.sign({email},secret_key,{expiresIn:'1h'})
-        console.log(secret_key)
-        return token
-    } catch (error) {
-        return error
-    }
-}
-module.exports={CreateToken}
+const CreateToken = (email) => {
+  try {
+    const secret_key=process.env.JWT_SECRET
+    const token = jwt.sign({ email }, secret_key, { expiresIn: "1h" });
+    console.log(secret_key);
+    return token;
+  } catch (error) {
+    return error;
+  }
+};
+module.exports = { CreateToken };
